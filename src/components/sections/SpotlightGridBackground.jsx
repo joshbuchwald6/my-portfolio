3152f5f2d0aed919db4d371a089d19d17fe7bb58
Josh Buchwald <joshbuchwald@gmail.com>
2025-04-10 22:52:24 -0400

    feat: add interactive cursor-following spotlight effect

--- src/components/sections/SpotlightGridBackground.jsx
+++ src/components/sections/SpotlightGridBackground.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styles from './SpotlightGridBackground.module.css'
 
 function SpotlightGridBackground ({ color = 'pink', className = '' }) {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+    return () => window.removeEventListener('mousemove', handleMouseMove)
+  }, [])
+
   return (
-    <div className={`${styles.background} ${className}`} style={{ '--spotlight-color': color }} />
+    <div className={`${styles.background} ${className}`} style={{ '--spotlight-color': color }}>
+      <div 
+        className={styles.spotlight}
+        style={{
+          '--x': `${mousePosition.x}px`,
+          '--y': `${mousePosition.y}px`
+        }}
+      />
+    </div>
   )
 }
 
